d35629cee41c97455dff2f6d160ba837f23d0386
Federico Rossi <federico.rossi@ing.unipi.it>
2021-07-02 08:06:01 +0200

    Fixed env script

--- env_buddy.sh
+++ env_buddy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -it  -v/home/federico/cppWorkspace/cppposit_private:/cppposit_private -v $PWD/tensorflow:/tensorposit -e HOST_PERMS="$(id -u):$(id -g)" tensorflow/tensorflow:devel bash
+docker run -it  -e BUILD_BUDDY_API_KEY=$BUILD_BUDDY_API_KEY -v/home/federico/cppWorkspace/cppposit_private:/cppposit_private -v $PWD/tensorflow:/tensorposit -e HOST_PERMS="$(id -u):$(id -g)" tensorflow/tensorflow:devel bash
 
 
 
