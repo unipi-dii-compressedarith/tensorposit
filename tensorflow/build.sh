6c8597c6ddab70ab01b9d503e7c29f21942d8b21
fabiano <fabiano@Fabiano>
2023-06-22 16:45:52 +0200

    Initial commit to publish the branch seai-project. Modified the env.sh file and the tensorflow/build.sh to add the resource limitation and remove the opt

--- tensorflow/build.sh
+++ tensorflow/build.sh
@@ -1,4 +1,4 @@
 #!bin/sh
-bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package
+bazel build --jobs=2 --local_ram_resources=4000 --local_cpu_resources=4 //tensorflow/tools/pip_package:build_pip_package
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt
 
